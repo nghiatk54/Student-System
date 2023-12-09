c88f2d04035d02e6012c8cc183905e51537bc71d
nghiatk54 <nghiatk54@gmail.com>
2023-12-09 15:14:38 +0700

    Create Model class BaseEntity

--- model/student.cpp
+++ model/student.cpp
@@ -1,39 +1,13 @@
+#include "baseEntity.cpp"
+
 // Class student
-class Student
+class Student : public BaseEntity
 {
 private:
-    int id;
-    string name;
-    int age;
-    string phoneNumber;
     double gpa;
 
 public:
     // Setter information
-    // Function setter ID
-    void setId(int id)
-    {
-        this->id = id;
-    }
-
-    // Function setter name
-    void setName(string name)
-    {
-        this->name = name;
-    }
-
-    // Function setter Age
-    void setAge(int age)
-    {
-        this->age = age;
-    }
-
-    // Function setter Phone number
-    void setPhoneNumber(string phoneNumber)
-    {
-        this->phoneNumber = phoneNumber;
-    }
-
     // Function setter GPA
     void setGpa(double gpa)
     {
@@ -41,30 +15,6 @@ public:
     }
 
     // Getter information
-    // Function get ID
-    int getId()
-    {
-        return id;
-    }
-
-    // Function get name
-    string getName()
-    {
-        return name;
-    }
-
-    // Function get Age
-    int getAge()
-    {
-        return age;
-    }
-
-    // Function get Phone number
-    string getPhoneNumber()
-    {
-        return phoneNumber;
-    }
-
     // Function get GPA
     double getGpa()
     {
--- model/teacher.cpp
+++ model/teacher.cpp
@@ -1,39 +1,13 @@
+#include "baseEntity.cpp"
+
 // Class Teacher
-class Teacher
+class Teacher : public BaseEntity
 {
 private:
-    int id;
-    string name;
-    int age;
-    string phoneNumber;
     double salary;
 
 public:
     // Setter information
-    // Function setter ID
-    void setId(int id)
-    {
-        this->id = id;
-    }
-
-    // Function setter name
-    void setName(string name)
-    {
-        this->name = name;
-    }
-
-    // Function setter Age
-    void setAge(int age)
-    {
-        this->age = age;
-    }
-
-    // Function setter Phone number
-    void setPhoneNumber(string phoneNumber)
-    {
-        this->phoneNumber = phoneNumber;
-    }
-
     // Function setter Salary
     void setSalary(double salary)
     {
@@ -41,30 +15,6 @@ public:
     }
 
     // Getter information
-    // Function get ID
-    int getId()
-    {
-        return id;
-    }
-
-    // Function get name
-    string getName()
-    {
-        return name;
-    }
-
-    // Function get Age
-    int getAge()
-    {
-        return age;
-    }
-
-    // Function get Phone number
-    string getPhoneNumber()
-    {
-        return phoneNumber;
-    }
-
     // Function get Salary
     double getSalary()
     {
