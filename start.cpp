85053ad2f9ab066cf100001746a2fd7f0b85ce9a
nghiatk54 <nghiatk54@gmail.com>
2023-12-12 16:23:19 +0700

    Create function displaySystem - showList

--- start.cpp
+++ start.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// Declare function display system
+void displaySystem();
+
+// Declare function show List
+void showList(string value);
+
 int main()
 {
     int process;
     int flag = 0;
     while (true)
     {
-        cout << "\t\t\t*********** Student Management System ***********" << endl;
-        cout << "Please Enter Your Process you need to do!" << endl;
-        cout << "1 - Add Student"
-             << "\t\t"
-             << "2 - Remove Student" << endl;
-        cout << "3 - Edit Student"
-             << "\t"
-             << "4 - Show Student" << endl;
-        cout << "5 - Exit" << endl;
+        displaySystem(); // Display system program
         cin >> process;
         switch (process)
         {
         case 1:
-            cout << "Process Add" << endl;
+            showList("Student"); // Show list Student
             break;
         case 2:
-            cout << "Process Remove" << endl;
+            showList("Course"); // Show list Course
             break;
         case 3:
-            cout << "Process Edit" << endl;
+            showList("Teacher"); // Show list Teacher
             break;
         case 4:
-            cout << "Process Show" << endl;
-            break;
-        case 5:
-            cout << "Process Exit" << endl;
             flag = 1;
             break;
         default:
@@ -43,4 +37,29 @@ int main()
             break;
         }
     }
+}
+
+// Declare function display system
+void displaySystem()
+{
+    cout << "\t\t\t*********** Student Management System ***********" << endl;
+    cout << "Please Enter Your Process you need to do!" << endl;
+    cout << "1 - About Students"
+         << "\t\t"
+         << "2 - About Courses" << endl;
+    cout << "3 - About Teachers"
+         << "\t\t"
+         << "4 - Exit" << endl;
+}
+
+// Function show list by value
+void showList(string value)
+{
+    cout << "1 - Add " << value
+         << "\t\t"
+         << "2 - Remove " << value << endl;
+    cout << "3 - Edit " << value
+         << "\t"
+         << "4 - Show " << value << endl;
+    cout << "5 - Exit" << endl;
 }
